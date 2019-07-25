63986880f4b7121bdef10188b176436f2d086ca3
tanlaunchdeck <tannguyen@tans-mbp.lan>
2019-07-25 18:31:47 +0700

    hot fix

--- constants/endPoint.js
+++ constants/endPoint.js
@@ -1,4 +1,4 @@
-//export const endPoint = "https://comigo-backend.herokuapp.com/api/"
-export const endPoint = "https://comigo-backend-dev.herokuapp.com/api/"
+export const endPoint = "https://comigo-backend.herokuapp.com/api/"
+//export const endPoint = "https://comigo-backend-dev.herokuapp.com/api/"
 //export const endPoint = "http://comigobackend-dev.us-west-1.elasticbeanstalk.com/api/"
 //export const endPoint = "http://comigobackend-production.us-west-1.elasticbeanstalk.com/api/"
